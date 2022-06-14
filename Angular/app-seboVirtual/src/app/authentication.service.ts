406fbbed897d36a786adfff234258ea64330ecff
Pamela-WMachado <pamela.wehle@gmail.com>
2022-06-14 11:56:28 -0300

    @PamelaWMachado - logout

--- Angular/app-seboVirtual/src/app/authentication.service.ts
+++ Angular/app-seboVirtual/src/app/authentication.service.ts
@@ -13,7 +13,6 @@ export class AuthenticationService {
   constructor(private http: HttpClient) {}
 
   logar(email: string, senha: string){
-    console.log(email,senha)
     return this.http.post('https://ms-usuarios.herokuapp.com/usuarios/login',{
       email: email,
       senha: senha
